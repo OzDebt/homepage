b8f4955b4b76f69be0cf2ed608b5b6a32855c69a
Atham Patel <atham@tendersoftware.in>
2018-09-14 12:26:14 +0530

    Completed the HTML Conversion with supplied design and QA also done with it.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@
 $(document).ready(function(){
 	"use strict";
 
+	var ua = window.navigator.userAgent;
+    var msie = 'user_agent';
+	if(ua.indexOf("MSIE") > -1){
+		msie = 'msie_agent';
+	}else if(ua.indexOf("Trident") > -1){
+		msie = 'msie_agent';
+	}
+	
+	console.log(msie);
+	
+	$(document.body).addClass(msie);
 	var window_width 	 = $(window).width(),
 	window_height 		 = window.innerHeight,
 	header_height 		 = $(".default-header").height(),
@@ -23,26 +34,69 @@ $(document).ready(function(){
     });
      
     $('select').niceSelect();
-    $('.img-pop-up').magnificPopup({
+   /* $('.img-pop-up').magnificPopup({
         type: 'image',
         gallery:{
         enabled:true
         }
-    });
+    });*/
+	
+	$('.request-demo').magnificPopup({
+		  delegate: 'a',
+		  removalDelay: 500, //delay removal by X to allow out-animation
+		  closeMarkup: '<a title="%title%" href="javascript:void(0)" class="mfp-close">X</a>',
+		  callbacks: {
+			beforeOpen: function() {
+			   this.st.mainClass = this.st.el.attr('data-effect');
+			   $(document.body).addClass('magnific-active');
+			},
+			afterClose: function() {
+				$(document.body).removeClass('magnific-active');
+			}
+		  },
+		  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+		});
 
-    $('.active-works-carousel').owlCarousel({
-        center: true,
-        items:1,
-        loop:true,
-        margin: 100,
-        dots: true
-    });
-    $('.active-banner-slider').owlCarousel({
-        items:1,
-        loop:true,
-        margin: 100,
-        dots: true
-    });
+	if($(document).find('.our-process-item').length > 0){		
+		$('.our-process-item').owlCarousel({
+				loop:true,
+				margin:10,
+				dots:true,
+				responsiveClass:true,
+				responsive:{
+					0:{
+						items:1,
+						
+					},
+					520:{
+						items:1,
+						nav:false,
+						dots:true,
+					},
+					768:{
+						items:3,
+						
+						dots:true,
+					},
+					850:{
+						items:4,
+						nav:false,
+						loop:false,
+						dots:false,
+					}
+				}
+			})
+	}
+	
+	jQuery(document).on('click', '.c-hamburger', function() {
+		jQuery(this).toggleClass('is-active');	
+		jQuery('.headers').toggleClass('active_menu');
+		jQuery(document.body).toggleClass('push-toright');
+		var $height = parseInt(jQuery(window).height());
+		jQuery("#nav-main").css('max-height',$height);
+		
+	});	
+
     // Add smooth scrolling to Menu links
 
     $(document).ready(function() {
